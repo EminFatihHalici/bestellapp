d2a22e3a0a917a27750ae63f7ad64f739876902d
Y********* <halicieminfatih@gmail.com>
2025-09-08 13:36:02 +0200

    add: added add button

--- scripts/template.js
+++ scripts/template.js
@@ -4,7 +4,6 @@ function getNotesHTML(dish) {
             <h3 class="dish-name">${dish.name}</h3>
             <p class="dish-description">${dish.description}</p>
             <p class="dish-price"><strong>${dish.price.toFixed(2)} €</strong></p>
-            
              <button class="hinzufügen">+</button>
         </div>
 
