72fd986eb8302a41d36e1e3e0df148a336575678
Y********* <halicieminfatih@gmail.com>
2025-09-08 13:37:22 +0200

    refactor and  fix: get the template in categorys and addinfg also titles

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ function init() {
 }
 
 function renderContent() {
-
     let contentDish = document.getElementById('content_div');
-    for (let dishIndex = 0; dishIndex < dishes.length; dishIndex++) {
-        contentDish.innerHTML += getNotesHTML(dishIndex);
-    }
+    contentDish.innerHTML = "";
+
+    let categories = [...new Set(dishes.map(d => d.category))];
+    let html = "";
+
+    categories.forEach(category => {
+        let categoryId = `category-${category.toLowerCase()}`;
+
+        html += `<h2 id="${categoryId}" class="category-title">${category.toUpperCase()}</h2>`;
+
+        let filteredDishes = dishes.filter(d => d.category === category);
+        filteredDishes.forEach(dish => {
+            html += getNotesHTML(dish);
+        });
+    });
+
+    contentDish.innerHTML = html;
 }
