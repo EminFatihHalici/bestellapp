3a72a7de453192c9fcea61f0ae1bf8d2b35c527c
Y********* <halicieminfatih@gmail.com>
2025-09-11 01:09:46 +0200

    style: added content into responsive basket

--- scripts/template.js
+++ scripts/template.js
@@ -30,7 +30,43 @@ function getBasket() {
 function getRespBasket() {
     return `
     
-        
+        <div class="basket_stickyrail">
+
+                <div class="basket">
+                    <h2 class="basket_head">Warenkorb</h2>
+                </div>
+
+                <div class="basket_food_content" id="basket_food_content"></div>
+
+                <table class="basket_order">
+                    <tr>
+                        <td class="basket_minus_icon"></td>
+                        <td>StückX</td>
+                        <td class="basket_plus_icon"></td>
+                        <td class="p_right p_left">Preis€</td>
+                        <td><img class="delete_bin" src="./assets/pictures/trash-can-10412.svg" alt="Löschen"></td>
+                    </tr>
+
+                </table>
+
+
+
+                <div class="total">
+                    <div class="p_top">Zwischensumme</div>
+                    <div class="p_top">Preis€</div>
+                </div>
+
+                <div class="total">
+                    <div class="p_top">Lieferkosten</div>
+                    <div class="p_top">Preis€</div>
+                </div>
+
+                <div class="total">
+                    <div class="p_top total_sum">Gesamt</div>
+                    <div class="p_top total_sum">Preis€</div>
+                </div>
+
+            </div>
     
     `
 }
