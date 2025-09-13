7c7ca7a1a32b0e4bfd06d9e33b832d59b0ca851c
Y********* <halicieminfatih@gmail.com>
2025-09-13 06:40:11 +0200

    test: outcommented function for test

--- script.js
+++ script.js
@@ -10,24 +10,24 @@ function getDishIndex() {
     let dishContentRef = document.getElementById('content_div')
     dishContentRef.innerHTML = "";
 
-    for (let index = 0; index < dishes.length; index++) {
-        dishContentRef.innerHTML += getNotesHTML(index);
+    for (let indexDishes = 0; indexDishes < dishes.length; indexDishes++) {
+        dishContentRef.innerHTML += getNotesHTML(indexDishes);
     }
 
 }
 
-function basketContent() {
-    let basketRef = document.getElementById("basket_food_content");
-    basketRef.innerHTML = getBasket();
-}
+// //function basketContent() {
+//     let basketRef = document.getElementById("basket_food_content");
+//     basketRef.innerHTML = getBasket();
+// //}
 
-function respBasketContent() {
-    let respBasketRef = document.getElementById("respBasket");
-    respBasketRef.innerHTML = getRespBasket();
-}
+// //function respBasketContent() {
+//     let respBasketRef = document.getElementById("respBasket");
+//     respBasketRef.innerHTML = getRespBasket();
+// //}
 
-function toggleOff() {
-    let overlayRef = document.getElementById("resp_toggle_off")
+// function toggleOff() {
+//     let overlayRef = document.getElementById("resp_toggle_off")
 
-    overlayRef.classList.toggle("d_none")
-}
+//     overlayRef.classList.toggle("d_none")
+// }
